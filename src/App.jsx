73504d6a9b1b1beae6355c84e8454ea815076b43
Soumyabrata Mukherjee <cseai2024042@rcciit.org.in>
2025-01-02 11:33:08 +0530

    Updated code with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from "react";
-import Toggle from "./toggle.jsx";
 import "./global.css";
+import Todo from "./Todo";
 
 const App = () => {
   return (
-    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] dark:bg-black dark:bg-[linear-gradient(to_right,#292924_1px,transparent_1px),linear-gradient(to_bottom,#292924_1px,transparent_1px)] bg-[size:6rem_4rem]">
-      <Toggle />
-      <div className="flex justify-center items-center h-screen text-center">
-        <div className="text-center">
-          <h1 className="mx-auto mb-4 max-w-3xl text-4xl/[1.1] font-extrabold tracking-tight text-foreground dark:text-white md:text-5xl/[1.1]">
-            Beautiful UI components built with Tailwind CSS and Next.js.
-          </h1>
-
-          <div className="mt-8">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="dark:text-white dark:bg-black w-full max-w-md p-3 border border-gray-300 dark:border-gray-600 rounded-lg shadow-md dark:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-300"
-            />
-          </div>
-        </div>
+    <div className="mt-10 absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
+      <div className="block mx-auto p-4 bg-white shadow-lg rounded-lg w-96">
+        <h1 class="mx-auto mb-4 max-w-3xl text-4xl/[1.1] font-extrabold tracking-tight text-foreground dark:text-white md:text-5xl/[1.1]">
+          Todo App Made With React
+        </h1>
+        <Todo />
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Default port is 5173, but you can change it here
+  },
+  build: {
+    outDir: "dist", // Default output directory for production build
+  },
+});
